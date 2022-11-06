13bb365286fd0510c905994552044e6aa13e1438
kelly cristina <kellycrisbarbosalima@gmail.com>
2022-11-06 11:28:20 -0300

    submissão do formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent { // tudo ocorre 
   title = 'busca-cep';
+
+// injeção de depedencias é feita dentro do construtor   
+  constructor(
+    private formBuilder : FormBuilder  //contruindo formulario
+  )
+  {}
+
+  formulario: FormGroup = this.formBuilder.group({
+    inputCep: ["", Validators.required]
+  })
+
+     procurar(){
+      const valorInputCep = this.formulario.get('inputCep')?.value
+  }
 }
